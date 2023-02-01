9bcace0dcb63f49f55c83dea221eea5ead9bf724
sean3 <cocoquiet@knu.ac.kr>
2023-02-01 18:24:01 +0900

    feat : Add get_notice_fast

--- crawler.py
+++ crawler.py
@@ -87,6 +87,64 @@ def get_notice(searchCategory='전체', amount=1, *dataTypes):
 
         return noticeList
 
+def get_notice_fast(searchCategory='전체', amount=1, *dataTypes):
+    global link, title, category, createDate
+
+    if searchCategory not in URLs:
+        raise ValueError('category must be one of 전체, 일반공지, 학사, 장학, 심컴, 글솝, 대학원, 대학원 계약학과')
+    
+    elif set(dataTypes) - set(['link', 'title', 'category', 'createDate']):
+        raise ValueError('data_type must be one of link, title, category, createDate')
+
+    else:
+        noticeList = []
+
+        response = requests.get(URLs[searchCategory])
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        limit = int(soup.select_one('tbody tr:not(.bo_notice) td.td_num2').text.strip())
+
+        if amount > limit:
+            amount = limit
+
+        if amount <= 15:
+            searchList = list(soup.find('tbody').find_all('tr', class_=lambda x: x != 'bo_notice'))
+
+            for idx in range(amount):
+                link = searchList[idx].select('div.bo_tit a')[0].get('href')
+                title = searchList[idx].select('div.bo_tit a')[0].text.strip()
+                category = searchList[idx].select('td.td_subject a.bo_cate_link')[0].text if searchCategory == '전체' else searchCategory
+                createDate = searchList[idx].find('td', class_='td_datetime hidden-xs').text
+
+                if dataTypes == ():
+                    noticeList.append([link, title, category, createDate])
+                else:
+                    noticeList.append([globals()[data] for data in dataTypes])
+
+        else:
+            pages = amount // 15 + 2
+
+            for page in range(1, pages):
+                response = requests.get(URLs[searchCategory] + '&page=' + str(page))
+                soup = BeautifulSoup(response.text, 'html.parser')
+                searchList = list(soup.find('tbody').find_all('tr', class_=lambda x: x != 'bo_notice'))
+
+                if searchList == []:
+                    break
+
+                for idx in range(15 if page != pages - 1 else amount % 15):
+                    link = searchList[idx].select('div.bo_tit a')[0].get('href')
+                    title = searchList[idx].select('div.bo_tit a')[0].text.strip()
+                    category = searchList[idx].select('td.td_subject a.bo_cate_link')[0].text if searchCategory == '전체' else searchCategory
+                    createDate = searchList[idx].find('td', class_='td_datetime hidden-xs').text
+
+                    if dataTypes == ():
+                        noticeList.append([link, title, category, createDate])
+                    else:
+                        noticeList.append([globals()[data] for data in dataTypes])
+
+        return noticeList
+
 if __name__ == '__main__':
     noticeList = get_notice('전체', 1000)
     print(len(noticeList))
