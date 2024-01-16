d96ee887fbcbe75ffd860004713f2cb01cd9492e
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2024-01-16 20:58:27 +0900

    main sync (#10)
    * refacor : change code naming & delete ValueError (#2)
    
    * Fix: 코드 컨벤션 수정 및 raise error 제거
    
    * Refactor: 변수명 케이스 변경
    
    * Refactor : dataTypes, globals 제거
    
    * crawlNoticeFromWeb(getDB -> crawlNoticeFromWeb) 메소드 리팩토링 및 크롤링 데이터 필드 추가 (#5)
    
    * Feature/update (#6)
    
    * Refactor: createTable, connectDB 메소드 통합(__init__)
    
    * Refactor: 코딩 컨벤션 변경
    
    * Feat: mysql 변경
    
    * Feat: db 클래스 분리
    
    * Feat: update_db 추가
    
    * Feat: data 이름 변경(num -> id_)
    
    * Feat: mysql 서버 변경 & DB 클래스 attribute 추가(db)
    
    * Feat: 테이블 스키마 변경(id, num 분리)
    
    * Docs: docstring 추가 & 크롤링 데이터 주석 추가
    
    * Feat: DB 메소드 변경(get_data_from_db -> get_date)
    
    * Docs: 크롤링 데이터 설명 변경(id_)
    
    * Docs: 크롤링 데이터 설명 변경(status)
    
    * Fix: get_data 반환값 수정(list[tuple] -> list[Notice])
    
    * Feat: api call 추가 & scheduler 설정
    
    * Refactor: 불필요한 코드 제거(DB.get_data(), pymysql)
    
    * Feat: convert_notice_list_to_json(), send_notice_to_api() 추가
    
    * Feat: Notice 속성 재정의(id_ 제거 & saved_at 추가)
    
    * Feat: schedule 추가
    
    * Refactor: saved_at, updated_at 제거
    
    * Style: __main__ 조건 추가
    
    * Style: nl 추가
    
    * send_notice_to_api() 로직 수정 (#8)
    
    * Fix: send_notice_to_api 수정(convert_notice_list_to_json 제거)
    
    * Feat: CATEGORY_ALIAS 추가
    
    * Refactor: DB 파일 제거
    
    * Refactor: Notice 속성 삭제
    
    * Feat: 앤드포인트 url 변경 & 크롤링 크기 변경
    
    * STYLE: 앤드포인트 URL 환경변수로 변경
    
    * 스케줄러 변경(python schedule -> linux crontab) (#9)
    
    * Style: schedule 제거
    
    * Style: 로그 정리(dict 제거 & 현재시간 추가)
    
    ---------
    
    Co-authored-by: cocoquiet <cocoquiet@knu.ac.kr>
    Co-authored-by: JiSeong Lee <52674782+easycastle@users.noreply.github.com>

--- crawler.py
+++ crawler.py
@@ -1,158 +1,108 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, PageElement
+import json
 
-import schedule
-
-import sqlite3
-
+from notice import *
 
 URLs = {
-    '전체': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1',
-    '일반공지': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EC%9D%BC%EB%B0%98%EA%B3%B5%EC%A7%80',
-    '학사': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%ED%95%99%EC%82%AC',
-    '장학': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EC%9E%A5%ED%95%99',
-    '심컴': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EC%8B%AC%EC%BB%B4',
-    '글솝': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EA%B8%80%EC%86%9D',
-    '대학원': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EB%8C%80%ED%95%99%EC%9B%90',
-    '대학원 계약학과': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EB%8C%80%ED%95%99%EC%9B%90+%EA%B3%84%EC%95%BD%ED%95%99%EA%B3%BC'
+        '전체': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1',
+        '일반공지': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EC%9D%BC%EB%B0%98%EA%B3%B5%EC%A7%80',
+        '학사': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%ED%95%99%EC%82%AC',
+        '장학': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EC%9E%A5%ED%95%99',
+        '심컴': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EC%8B%AC%EC%BB%B4',
+        '글솝': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EA%B8%80%EC%86%9D',
+        '대학원': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EB%8C%80%ED%95%99%EC%9B%90',
+        '대학원 계약학과': 'https://computer.knu.ac.kr/bbs/board.php?bo_table=sub5_1&sca=%EB%8C%80%ED%95%99%EC%9B%90+%EA%B3%84%EC%95%BD%ED%95%99%EA%B3%BC'
+    }
+
+CATEGORY_ALIAS = {
+    '전체' : 'ALL',
+    '일반공지' : 'NORMAL',
+    '학사' : 'STUDENT',
+    '장학' : 'SCHOLARSHIP',
+    '심컴' : 'SIM_COM',
+    '글솝' : 'GL_SOP',
+    '인컴' : 'SIM_COM',
+    '대학원' : 'GRADUATE_SCHOOL',
+    '대학원 계약학과' : 'GRADUATE_CONTRACT'
 }
 
-num = None
-link = None
-title = None
-category = None
-createDate = None
-content = None
+MAX_NOTICE_SIZE = 15
 
 
-def create_table():
-    conn = sqlite3.connect('notice.db')
-    c = conn.cursor()
-
-    c.execute('''CREATE TABLE IF NOT EXISTS Notice
-                (num INTEGER PRIMARY KEY, link TEXT, title TEXT, category TEXT, createDate DateTime, content LONGTEXT)''')
-
-    conn.commit()
-    conn.close()
-
-def connectDB():
-    conn = sqlite3.connect('notice.db')
-    c = conn.cursor()
-
-    return conn, c
-
-def get_notice(searchCategory='전체', amount=1, *dataTypes):
-    global num, link, title, category, createDate, content
-
-    if searchCategory not in URLs:
-        raise ValueError('category must be one of 전체, 일반공지, 학사, 장학, 심컴, 글솝, 대학원, 대학원 계약학과')
-    
-    elif set(dataTypes) - set(['num', 'link', 'title', 'category', 'createDate', 'content']):
-        raise ValueError('data_type must be one of num, link, title, category, createDate, content')
-
-    else:
-        noticeList = []
-
-        response = requests.get(URLs[searchCategory])
+class Crawler:
+    def __parse_notice_total_count(self) -> int:
+        response = requests.get(URLs['전체'])
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        limit = int(soup.select_one('tbody tr:not(.bo_notice) td.td_num2').text.strip())
+        return int(soup.select_one('tbody tr:not(.bo_notice) td.td_num2').text.strip())
 
-        if amount > limit:
-            amount = limit
+    def __parse_notice_table(self, search_category, page) -> list[PageElement]:
+        response = requests.get(URLs[search_category] + '&page=' + str(page))
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        if amount <= 15:
-            searchList = list(soup.select('tbody tr:not(.bo_notice) td.td_subject div.bo_tit a'))
+        return list(soup.select('tbody tr:not(.bo_notice) td.td_subject div.bo_tit a'))
 
-            for idx in range(amount):
-                num = limit - idx
-                link = searchList[idx].get('href')
+    def __get_notice_data(self, notice: PageElement) -> Notice:
+        link = notice.get('href')
+        num = int(link.split('wr_id')[-1].split('&')[0].replace('=', ''))
 
-                response = requests.get(link)
-                soup = BeautifulSoup(response.text, 'html.parser')
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-                title = soup.select_one('.bo_v_tit').text.strip()
-                category = soup.select_one('.bo_v_cate').text if searchCategory == '전체' else searchCategory
-                createDate = '20' + soup.select_one('.if_date').text.replace('작성일 ', '') + ':00'
-                content = soup.select_one('#bo_v_con').text.strip().replace('\xa0', '')
+        title = soup.select_one('.bo_v_tit').text.strip()
+        category = CATEGORY_ALIAS[soup.select_one('.bo_v_cate').text]
+        content = soup.select_one('#bo_v_con').text.strip().replace('\xa0', '')
+        created_at = '20' + soup.select_one('.if_date').text.replace('작성일 ', '') + ':00'
 
-                if dataTypes == ():
-                    noticeList.append([link, title, category, createDate, content])
-                else:
-                    noticeList.append([globals()[data] for data in dataTypes])
+        return Notice(num, link, title, category, content, created_at)
 
-        else:
-            pages = amount // 15 + 2
+    def crawl_notice_from_web(self, search_category: str='전체', amount: int=-1) -> list[Notice]:
+        """공지사항을 크롤링하는 함수
 
-            for page in range(1, pages):
-                response = requests.get(URLs[searchCategory] + '&page=' + str(page))
-                soup = BeautifulSoup(response.text, 'html.parser')
-                searchList = list(soup.select('tbody tr:not(.bo_notice) td.td_subject div.bo_tit a'))
+        Args:
+            search_category (str, optional): 크롤링할 공지사항의 카테고리. Defaults to '전체'.
+            amount (int, optional): 크롤링할 공지사항의 개수. Defaults to -1.
 
-                if searchList == []:
-                    break
+        Returns:
+            list[Notice]: 크롤링한 공지사항 리스트
+        """
+        
+        if amount == 0:
+            return []
 
-                for idx in range(15 if page != pages - 1 else amount % 15):
-                    num = limit - (page - 1) * 15 - idx
-                    link = searchList[idx].get('href')
+        notice_list = list()
 
-                    response = requests.get(link)
-                    soup = BeautifulSoup(response.text, 'html.parser')
+        notice_total_count = self.__parse_notice_total_count()
+        if amount > notice_total_count or amount == -1:
+            amount = notice_total_count
 
-                    title = soup.select_one('.bo_v_tit').text.strip()
-                    category = soup.select_one('.bo_v_cate').text if searchCategory == '전체' else searchCategory
-                    createDate = '20' + soup.select_one('.if_date').text.replace('작성일 ', '') + ':00'
-                    content = soup.select_one('#bo_v_con').text.strip().replace('\xa0', '')
+        pages = amount // MAX_NOTICE_SIZE + 2
 
-                    if dataTypes == ():
-                        noticeList.append([num, link, title, category, createDate, content])
-                    else:
-                        noticeList.append([globals()[data] for data in dataTypes])
+        for page in range(1, pages):
+            notice_table = self.__parse_notice_table(search_category, page)
 
-        return noticeList
+            if page == pages - 1:
+                notice_table = notice_table[:amount % MAX_NOTICE_SIZE]
 
-def insert_notice(noticeList):
-    conn, c = connectDB()
+            for notice in notice_table:
+                notice_list.append(self.__get_notice_data(notice))
 
-    for notice in noticeList:
-        c.execute('INSERT INTO Notice VALUES (?, ?, ?, ?, ?, ?)', notice)
+        return notice_list
 
-    conn.commit()
-    conn.close()
+    def send_notice_to_api(self, url: str, notice_list: list[Notice]) -> int:
+        """크롤링한 공지사항을 api로 전송하는 함수
 
-def get_notice_from_db(searchCategory='전체', amount=1, *dataTypes):
-    conn, c = connectDB()
+        Args:
+            url (str): api의 url
+            notice_list (list[Notice]): 전송할 공지사항 리스트
+        """
 
-    if searchCategory not in URLs:
-        raise ValueError('category must be one of 전체, 일반공지, 학사, 장학, 심컴, 글솝, 대학원, 대학원 계약학과')
-    
-    elif set(dataTypes) - set(['num', 'link', 'title', 'category', 'createDate', 'content']):
-        raise ValueError('data_type must be one of num, link, title, category, createDate, content')
+        response = requests.post(
+            url, 
+            json={'data': [notice.__dict__ for notice in notice_list]}, 
+            headers={'Content-Type': 'application/json'}
+            )
 
-    else:
-        if dataTypes == ():
-            c.execute('SELECT * FROM Notice WHERE category = ? ORDER BY createDate DESC LIMIT ?', (searchCategory, amount))
-        else:
-            c.execute('SELECT ' + ', '.join(dataTypes) + ' FROM Notice WHERE category = ? ORDER BY createDate DESC LIMIT ?', (searchCategory, amount))
-
-        noticeList = c.fetchall()
-
-        conn.commit()
-        conn.close()
-
-        return noticeList
-
-def updateDB():
-    conn, c = connectDB()
-
-    c.execute('SELECT num FROM Notice ORDER BY num DESC LIMIT 1')
-    lastNum = c.fetchone()[0]
-
-    noticeList = get_notice(amount=30)
-
-    for noticeIndx in range(noticeList[0][0] - lastNum):
-        c.execute('INSERT INTO Notice VALUES (?, ?, ?, ?, ?, ?)', noticeList[noticeIndx])
-
-
-if __name__ == '__main__':
-    create_table()
+        return response
+        
---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+import os
+from datetime import datetime
+
+from crawler import Crawler
+from notice import *
+
+crawler = Crawler()
+url = os.environ['API_URL']
+
+def run():
+    print(f'{datetime.now()} - Start crawling')
+    notice_list = crawler.crawl_notice_from_web(amount=100)
+    print(f'{datetime.now()} - Finish crawling')
+    response = crawler.send_notice_to_api(url, notice_list)
+    print(f'{datetime.now()} - Finish sending - {response}')
+
+if __name__ == '__main__':
+    run()
